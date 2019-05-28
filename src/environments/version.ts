fa93fd9903f1a345ac4f0ca4b42780767c7a2bc1
skyquartam <marcello.quarta.dechit@skytv.it>
2019-05-28 18:27:10 +0200

    v0.4

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,14 +1,27 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "895ae0e",
-    "hash": "895ae0e",
-    "distance": null,
-    "tag": null,
-    "semver": null,
-    "suffix": "895ae0e",
-    "semverString": null,
+    "dirty": true,
+    "raw": "v0.0.3-1-g24b8192-dirty",
+    "hash": "g24b8192",
+    "distance": 1,
+    "tag": "v0.0.3",
+    "semver": {
+        "options": {
+            "loose": false,
+            "includePrerelease": false
+        },
+        "loose": false,
+        "raw": "v0.0.3",
+        "major": 0,
+        "minor": 0,
+        "patch": 3,
+        "prerelease": [],
+        "build": [],
+        "version": "0.0.3"
+    },
+    "suffix": "1-g24b8192-dirty",
+    "semverString": "0.0.3+1.g24b8192.dirty",
     "version": "0.0.3"
 };
 /* tslint:enable */
